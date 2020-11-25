782086cca40dbb6a43b9e123da33f6bc0cb5fb7f
markomark98 <marco.marchionno@gmail.com>
2020-11-25 17:54:54 +0100

    rebase del servere affinché funzioni su ip locale

--- api/__init__.py
+++ api/__init__.py
@@ -11,4 +11,4 @@ from memorydump_component.volatility_api import volatility_api
 app.register_blueprint(volatility_api, url_prefix = "/volatility")
 
 if __name__ == '__main__':
-    app.run(host = '10.0.2.15')
+    app.run(debug = True)
