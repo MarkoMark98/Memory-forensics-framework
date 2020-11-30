b137f6feec766f11f2e252be3ad5b5954afdf517
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2020-11-30 20:16:49 +0100

    be update

--- api/be_component/be_api.py
+++ api/be_component/be_api.py
@@ -1,8 +1,12 @@
 from flask import Flask, Blueprint, current_app
 from flask_restful import Api, Resource
+import os
 
 be_api = Blueprint('be_api', __name__,  static_folder="static")
 
 @be_api.route("/<keyword>")
 def be_search(keyword):
+    #call to bulk estractor to carve files
+    #os.system("bulk_extractor -o outDir ../../memdump.zip")
+    
     return keyword
