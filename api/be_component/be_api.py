56471be650d1cbf491bd6b4a0fb2caec9edd8288
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2020-12-01 01:41:20 +0100

    Update be_api.py

--- api/be_component/be_api.py
+++ api/be_component/be_api.py
@@ -1,12 +1,16 @@
-from flask import Flask, Blueprint, current_app
+from flask import Flask, Blueprint, current_app ,request
 from flask_restful import Api, Resource
 import os
+import json
 
 be_api = Blueprint('be_api', __name__,  static_folder="static")
 
-@be_api.route("/<keyword>")
-def be_search(keyword):
+@be_api.route("",methods=["POST"])
+def be_search():
     #call to bulk estractor to carve files
     #os.system("bulk_extractor -o outDir ../../memdump.zip")
+
+    #Getting keywords from request body
+    dt = request.json
     
-    return keyword
+    return dt["keywords"]
